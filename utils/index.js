be2ba6d343975d0f2d5df9c27a1e985b362f5831
kuldeep <kuldeep@MacBook-Air.local>
2018-08-07 20:35:21 +0530

    Chnaged Urls to staging

--- utils/index.js
+++ utils/index.js
@@ -1,8 +1,8 @@
-export const ACC_API = 'https://accapi.appbase.io';
-export const SCALR_API = 'https://scalr.api.appbase.io';
+// export const ACC_API = 'https://accapi.appbase.io';
+// export const SCALR_API = 'https://scalr.api.appbase.io';
 export const BILLING_API = 'https://transactions.appbase.io';
-// export const ACC_API = 'https://accapi-staging.bottleneck.io';
-// export const SCALR_API = 'https://api-staging.bottleneck.io';
+export const ACC_API = 'https://accapi-staging.bottleneck.io';
+export const SCALR_API = 'https://api-staging.bottleneck.io';
 
 export function getCredentials(appId) {
 	return new Promise((resolve, reject) => {
