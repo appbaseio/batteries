5e4acab46205f24cdb7208f1b6d651d21ebc11f7
jyash97 <jyash97@gmail.com>
2018-08-22 18:24:53 +0530

    Add support for all props in Sandbox Component

--- components/SearchSandbox/index.js
+++ components/SearchSandbox/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Menu, Button, Dropdown, Icon, Modal, Input, Affix } from 'antd';
+import { Menu, Button, Dropdown, Icon, Modal, Input } from 'antd';
 import { css } from 'emotion';
 import { getParameters } from 'codesandbox/lib/api/define';
 
@@ -293,22 +293,6 @@ export default class SearchSandbox extends Component {
 							{props => React.cloneElement(child, { ...props })}
 						</SandboxContext.Consumer>
 					))}
-					{this.props.message ? (
-						<Affix
-							style={{
-								position: 'fixed',
-								bottom: '10px',
-								right: '35px',
-								padding: '5px',
-								background: 'white',
-								boxShadow: '0px 0px 1px rgba(0,0,0,.2)',
-							}}
-						>
-							{this.props.message.link ? <a href={this.props.message.link} rel="noopener noreferrer" target="_blank">
-								{this.props.message.text}
-                                  </a> : this.props.message.text}
-						</Affix>
-					) : null}
 				</div>
 
 				<Modal
--- components/SearchSandbox/utils/reactivelist-types.js
+++ components/SearchSandbox/utils/reactivelist-types.js
@@ -25,4 +25,35 @@ export default {
 		input: 'bool',
 		default: true,
 	},
+	pages: {
+		label: 'Number of Pages',
+		description: 'Number of user selectable pages to be displayed when pagination is enabled. ',
+		input: 'number',
+		default: 5,
+	},
+	paginationAt: {
+		label: 'Pagination position',
+		description: 'Determines the position where to show the pagination.',
+		input: 'dropdown',
+		options: [{ label: 'Top', key: 'top' }, { label: 'Bottom', key: 'bottom' }, { label: 'Both', key: 'both' }],
+		default: 'bottom',
+	},
+	showResultStats: {
+		label: 'Show Result Stats',
+		description: 'Whether to show result stats in the form of results found and time taken. ',
+		input: 'bool',
+		default: true,
+	},
+	stream: {
+		label: 'Stream',
+		description: 'Whether to stream new result updates in the UI.',
+		input: 'bool',
+		default: false,
+	},
+	loader: {
+		label: 'Loading Message',
+		description: '',
+		input: 'string',
+		default: '',
+	},
 };
