64e8007ff72e0afbf918289d4c2d3c07eca75532
Siddharth Kothari <sids.aquarius@gmail.com>
2018-08-29 05:54:08 +0530

    Update multilist-types.js

--- components/SearchSandbox/utils/multilist-types.js
+++ components/SearchSandbox/utils/multilist-types.js
@@ -1,8 +1,8 @@
 export default {
 	dataField: {
 		label: 'Data Field',
-		description: 'Select the field you want to generate filter for',
-		types: ['keyword', 'integer'],
+		description: 'Select the field for which you want to generate this filter for.',
+		types: ['keyword', 'integer', 'float', 'double', 'date'],
 		input: 'dropdown',
 		multiple: false,
 	},
@@ -13,53 +13,40 @@ export default {
 	},
 	size: {
 		label: 'Size',
-		description: 'Total number of list items to fetch',
+		description: 'Total number of list items to fetch.',
 		input: 'number',
 		default: 100,
 	},
 	sortBy: {
 		label: 'Sort By',
-		description: 'Sort the results by either Ascending or Descending order.',
+		description: 'Sort the results by either Count, Ascending or Descending order.',
 		input: 'dropdown',
-		options: [{ label: 'Ascending', key: 'asc' }, { label: 'Descending', key: 'desc' }],
-		default: 'asc',
+		options: [{ label: 'Count', key: 'count' }, { label: 'Ascending', key: 'asc' }, { label: 'Descending', key: 'desc' }],
+		default: 'count',
 	},
-	queryFormat: {
-		label: 'Query Format',
-		description: 'Sets the query format, can be or or and. Defaults to or.',
-		input: 'dropdown',
-		options: [{ label: 'Or', key: 'or' }, { label: 'And', key: 'and' }],
-		default: 'or',
-	},
-	showCheckbox: {
-		label: 'Show Checkbox',
-		description: 'Show checkbox icon for each list item.',
+	showSearch: {
+		label: 'Show Search',
+		description: 'Show a search bar to filter list items.',
 		input: 'bool',
 		default: true,
 	},
 	showCount: {
 		label: 'Show Count',
-		description: 'This will show a count of the number of occurences besides each list item',
+		description: 'Show a frequency count next to each list item.',
 		input: 'bool',
 		default: true,
 	},
-	showSearch: {
-		label: 'Show search',
-		description: 'This will render the search component within MultiList',
+	showCheckbox: {
+		label: 'Show Checkbox',
+		description: 'Show a checkbox icon next to each list item.',
 		input: 'bool',
 		default: true,
 	},
-	showFilter: {
-		label: 'Show Filter',
-		description: 'Show as filter when a value is selected in a global selected filters view.',
-		input: 'bool',
-		default: true,
-	},
-	URLParams: {
-		label: 'URLParams',
-		description:
-			'Enable creating a URL query string parameter based on the selected value of the list. This is useful for sharing URLs with the component state. ',
-		input: 'bool',
-		default: false,
-	},
+	queryFormat: {
+		label: 'Match All or Any',
+		description: 'When multiple values are selected, enable either match All values or match Any values.',
+		input: 'dropdown',
+		options: [{ label: 'Any', key: 'or' }, { label: 'All', key: 'and' }],
+		default: 'or',
+	}
 };
