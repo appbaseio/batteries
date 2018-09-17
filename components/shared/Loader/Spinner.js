033b8c5d240c86c39e2b5ed11211c13307d45c46
kuldeep <kuldepsaxena155@gmail.com>
2018-09-17 19:01:52 +0530

    fix(): Minor Fixes

--- components/shared/Loader/Spinner.js
+++ components/shared/Loader/Spinner.js
@@ -3,9 +3,9 @@ import { Icon, Spin } from 'antd';
 import Flex from '../Flex';
 
 export default (props) => {
-	const antIcon = <Icon type="loading" style={{ fontSize: 50, marginTop: '250px' }} spin />;
+	const antIcon = <Icon type="loading" style={{ fontSize: 50 }} spin />;
 	return (
-		<Flex justifyContent="center" alignItems="center" {...props}>
+		<Flex justifyContent="center" alignItems="center" css="min-height: 300px;" {...props}>
 			<Spin indicator={antIcon} />
 		</Flex>
 	);
