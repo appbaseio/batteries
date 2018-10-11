3459f74673d8a520cfd6b62feb4eb9b562cf74ff
jyash97 <jyash97@gmail.com>
2018-10-11 13:13:24 +0530

    Fix : Add sortBy in reactivelist types

--- components/SearchSandbox/utils/reactivelist-types.js
+++ components/SearchSandbox/utils/reactivelist-types.js
@@ -19,6 +19,17 @@ export default {
 		input: 'dropdown',
 		multiple: false,
 	},
+	sortBy: {
+		label: 'Sort Order',
+		description: 'Sort the results by one of Best Match, Ascending or Descending order.',
+		input: 'dropdown',
+		options: [
+			{ label: 'Best Match', key: '_score' },
+			{ label: 'Ascending', key: 'asc' },
+			{ label: 'Descending', key: 'desc' },
+		],
+		default: '_score',
+	},
 	size: {
 		label: 'Size',
 		description: 'Total number of results to fetch (maximum value is 1000).',
