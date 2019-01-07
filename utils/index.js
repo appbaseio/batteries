4da3fab7d01997eb25b49280fe9c21f3ad52772d
Siddharth Kothari <sids.aquarius@gmail.com>
2019-01-07 21:56:09 +0530

    update staging backend URLs

--- utils/index.js
+++ utils/index.js
@@ -3,8 +3,8 @@ import { doGet } from './requestService';
 
 // export const ACC_API = 'https://accapi.appbase.io';
 // export const SCALR_API = 'https://scalr.api.appbase.io';
-export const ACC_API = 'https://accapi-staging.bottleneck.io';
-export const SCALR_API = 'https://api-staging.bottleneck.io';
+export const ACC_API = 'https://accapi-staging.reactiveapps.io';
+export const SCALR_API = 'https://api-staging.reactiveapps.io';
 
 // Get credentials if permissions are already present
 export function getCredentialsFromPermissions(permissions = []) {
