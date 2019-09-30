66f2b7523d23290ecb6726ed6c5313588ac67fb5
ShahAnuj2610 <anujshah584@gmail.com>
2019-09-30 20:05:54 +0530

    feat: save analytics in preferences endpoint (#162)
    * feat: save analytics in preferences
    
    * format: capitalize
    
    * fix: handle analytics in setPreferences through state
    
    * fix: handle default analytics val
    
    * fix: analytics in getLocalPref

--- components/SearchSandbox/index.js
+++ components/SearchSandbox/index.js
@@ -70,6 +70,7 @@ class SearchSandbox extends Component {
 						this.setState({
 							profileList,
 							loading: false,
+							analytics: this.pref.analytics === undefined ? true : this.pref.analytics,
 						});
 					} else {
 						this.setState({
@@ -79,6 +80,7 @@ class SearchSandbox extends Component {
 							filterCount: Object.keys(componentProps).filter(
 								item => item !== 'search' && item !== 'result',
 							).length,
+							analytics: this.pref.analytics === undefined ? true : this.pref.analytics,
 						});
 					}
 				})
@@ -131,6 +133,7 @@ class SearchSandbox extends Component {
 			filterCount: Object.keys(componentProps).filter(
 				item => item !== 'search' && item !== 'result',
 			).length,
+			analytics: this.pref.analytics === undefined ? true : this.pref.analytics,
 		});
 	};
 
@@ -157,14 +160,17 @@ class SearchSandbox extends Component {
 	}
 
 	toggleAnalytics = () => {
-		this.setState(prevState => ({
-			analytics: !prevState.analytics,
-		}));
+		this.setState(
+			prevState => ({
+				analytics: !prevState.analytics,
+			}),
+			this.savePreferences,
+		);
 	};
 
 	savePreferences = (forceUpdate) => {
 		const { isDashboard, appName, clearProfile } = this.props;
-		const { profile, componentProps } = this.state;
+		const { profile, componentProps, analytics } = this.state;
 		const filteredProps = {};
 		Object.keys(componentProps).forEach((item) => {
 			const { defaultValue, value, ...state } = componentProps[item];
@@ -173,6 +179,7 @@ class SearchSandbox extends Component {
 		this.pref = {
 			...this.pref,
 			[profile]: filteredProps,
+			analytics,
 		};
 
 		if (isDashboard && (forceUpdate || !this.isUnsaved)) {
