077b53d80d39872e00020fec12877b00ed22773a
anuj <anujshah584@gmail.com>
2019-09-30 20:00:12 +0530

    fix: analytics in getLocalPref

--- components/SearchSandbox/index.js
+++ components/SearchSandbox/index.js
@@ -133,7 +133,7 @@ class SearchSandbox extends Component {
 			filterCount: Object.keys(componentProps).filter(
 				item => item !== 'search' && item !== 'result',
 			).length,
-			analytics: this.pref.analytics,
+			analytics: this.pref.analytics === undefined ? true : this.pref.analytics,
 		});
 	};
 
