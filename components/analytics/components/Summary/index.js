30eb49e1f7322064e2d12c40a72e9600a8ce1f2e
jyash97 <jyash97@gmail.com>
2019-10-03 12:35:08 +0530

    feat: add new summary UI

--- components/analytics/components/Summary/index.js
+++ components/analytics/components/Summary/index.js
@@ -1,13 +1,55 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import get from 'lodash/get';
+import { Row, Col, Icon } from 'antd';
 import PropTypes from 'prop-types';
+import { css } from 'emotion';
 import { getAppAnalyticsSummaryByName } from '../../../../modules/selectors';
 import { getAppAnalyticsSummary } from '../../../../modules/actions';
 import Loader from '../../../shared/Loader/Spinner';
 import { displayErrors } from '../../../../utils/heplers';
-import SummaryCard from './SummaryCard';
 import Flex from '../../../shared/Flex';
+
+const cardStyle = css`
+	margin-bottom: 10px;
+	background: rgba(229, 230, 233, 0.21);
+	padding: 15px 0;
+	p,
+	h2 {
+		margin: 0;
+		text-align: center;
+	}
+
+	p {
+		font-size: 1.2em;
+		font-weight: bold;
+		color: #8c8c8c;
+	}
+
+	h2 {
+		color: #595959;
+	}
+`;
+
+const cardContainer = css`
+	padding: 10px;
+`;
+
+const SummaryCard = ({
+ icon, title, count, style,
+}) => (
+	<Flex alignItems="center" justifyContent="center" style={style} className={cardStyle}>
+		{/* {icon && (
+			<div>
+				<Icon type={icon} style={{ fontSize: '2.5em', marginRight: 15 }} />
+			</div>
+		)} */}
+		<div>
+			<p>{title}</p>
+			<h2>{count}</h2>
+		</div>
+	</Flex>
+);
 
 class Summary extends React.Component {
 	componentDidMount() {
@@ -27,16 +69,80 @@ class Summary extends React.Component {
 			avgClickRate,
 			avgConversionRate,
 			totalSearches,
+			totalResults,
+			totalUsers,
+			avgSuggestionClicks,
 		} = this.props;
 		if (isLoading) {
 			return <Loader />;
 		}
 		return (
-			<Flex flexDirection="row" css="flex-wrap:wrap">
-				<SummaryCard title="Total Searches" count={totalSearches} border="#00f68e" />
-				<SummaryCard title="Average Click Rate" count={avgClickRate} border="#1A74FF" />
-				<SummaryCard title="Conversion" count={avgConversionRate} border="#C944FF" />
-			</Flex>
+			<Row>
+				<Col xl={8} md={12}>
+					<Row gutter={8} className={cardContainer}>
+						<Col span={24}>
+							<SummaryCard
+								icon="search"
+								style={{ borderTop: '2px solid #2f54eb', background: '#f0f5ff' }}
+								title="Total Searches"
+								count={totalSearches}
+							/>
+						</Col>
+						<Col sm={24} xs={24} xl={12}>
+							<SummaryCard
+								title="Total Users"
+								style={{ background: '#f0f5ff' }}
+								count={totalUsers}
+							/>
+						</Col>
+						<Col sm={24} xs={24} xl={12}>
+							<SummaryCard
+								title="Impressions"
+								count={totalResults}
+								style={{ background: '#f0f5ff' }}
+							/>
+						</Col>
+					</Row>
+				</Col>
+				<Col xl={8} md={12}>
+					<Row gutter={8} className={cardContainer}>
+						<Col span={24}>
+							<SummaryCard
+								icon="check"
+								title="Clicks"
+								style={{ borderTop: '2px solid #eb2f96', background: '#fff0f6' }}
+								count={avgClickRate}
+							/>
+						</Col>
+						<Col sm={24} xs={24} xl={12}>
+							<SummaryCard
+								title="Suggestion Clicks"
+								style={{ background: '#fff0f6' }}
+								count={avgSuggestionClicks}
+							/>
+						</Col>
+						<Col sm={24} xs={24} xl={12}>
+							<SummaryCard
+								title="Result Clicks"
+								style={{ background: '#fff0f6' }}
+								count={avgClickRate - avgSuggestionClicks}
+							/>
+						</Col>
+					</Row>
+				</Col>
+				<Col xl={8}>
+					<Row gutter={8} className={cardContainer}>
+						<Col span={24}>
+							<SummaryCard
+								icon="stock"
+								style={{ background: '#f6ffed', borderTop: '2px solid #52c41a' }}
+								title="Conversion"
+								count={avgConversionRate}
+							/>
+						</Col>
+					</Row>
+				</Col>
+			</Row>
 		);
 	}
 }
@@ -53,7 +159,11 @@ const mapStateToProps = (state) => {
 	return {
 		avgClickRate: get(appSummary, 'summary.avg_click_rate', 0),
 		avgConversionRate: get(appSummary, 'summary.avg_conversion_rate', 0),
+		avgSuggestionClicks: get(appSummary, 'summary.avg_suggestions_click_rate', 0),
+		noResultsRate: get(appSummary, 'summary.no_results_rate', 0),
 		totalSearches: get(appSummary, 'summary.total_searches', 0),
+		totalUsers: get(appSummary, 'summary.total_users', 0),
+		totalResults: get(appSummary, 'summary.total_results_count', 0),
 		isLoading: get(state, '$getAppAnalyticsSummary.isFetching'),
 		errors: [get(state, '$getAppAnalyticsSummary.error')],
 	};
