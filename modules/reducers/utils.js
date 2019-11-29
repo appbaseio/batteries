9d74ddf4693341426fbcd1a2671fb0e7511dda52
bietkul <kuldepsaxena155@gmail.com>
2019-11-30 01:37:57 +0530

    fix: minor issues with billing

--- modules/reducers/utils.js
+++ modules/reducers/utils.js
@@ -19,7 +19,7 @@ export const computePlan = ({ payload }) => {
 		isArcEnterprise,
 		isHostedArc,
 		isClusterBilling,
-		isPaid: !!payload.tier && payload.time_validity > 0,
+		isPaid: !payload.billing_type || (!!payload.tier && payload.time_validity > 0),
 		plan: getPlanFromTier(payload.tier),
 		daysLeft,
 	};
