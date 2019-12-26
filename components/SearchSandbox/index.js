9e0b2a06ff10790e6f0accddd16a6973a06cfd96
jyash97 <jyash97@gmail.com>
2019-12-26 21:51:00 +0530

    feat: upgrade rs version

--- components/SearchSandbox/index.js
+++ components/SearchSandbox/index.js
@@ -330,7 +330,7 @@ class SearchSandbox extends Component {
 							react: '16.8.6',
 							'react-dom': '16.8.6',
 							antd: '^3.6.6',
-							'@appbaseio/reactivesearch': '3.0.2',
+							'@appbaseio/reactivesearch': '3.1.3',
 							'react-expand-collapse': 'latest',
 						},
 					},
