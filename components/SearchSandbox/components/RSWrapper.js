7c815c42b561da9773ac4e497e4c2f17463e3731
Yash Joshi <jyash97@gmail.com>
2020-01-02 12:00:34 +0530

    feat: update search preview UI (#177)
    * feat: update search preview UI
    
    * fix: update icons style

--- components/SearchSandbox/components/RSWrapper.js
+++ components/SearchSandbox/components/RSWrapper.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import {
- Row, Col, Form, Button, Modal, Popover, message,
-} from 'antd';
+import { Row, Col, Form, Button, Modal, Popover, message, Icon } from 'antd';
 
 import {
- DataSearch, MultiList, ReactiveList, CategorySearch,
+	DataSearch,
+	MultiList,
+	ReactiveList,
+	CategorySearch,
 } from '@appbaseio/reactivesearch';
 
 import dataSearchTypes from '../utils/datasearch-types';
@@ -19,9 +20,12 @@ import DataFieldInput from './DataFieldInput';
 import { SandboxContext } from '../index';
 import getComponentProps from '../utils/getComponentProps';
 import {
- NumberInput, TextInput, DropdownInput, ToggleInput,
+	NumberInput,
+	TextInput,
+	DropdownInput,
+	ToggleInput,
 } from '../../shared/Input';
-import { formWrapper } from '../styles';
+import { formWrapper, componentStyle } from '../styles';
 import HtmlEditor from './HtmlEditor';
 
 const componentMap = {
@@ -57,7 +61,11 @@ class RSComponentRender extends Component {
 		if (!props.componentProps.dataField) {
 			// set default dataField for the component if not defined
 			const { component, id, mappings } = props;
-			const dataFields = getAvailableDataField({ component, id, mappings });
+			const dataFields = getAvailableDataField({
+				component,
+				id,
+				mappings,
+			});
 			const { multiple } = propsMap[component].dataField;
 			let otherProps = {};
 			if (props.id === 'search') {
@@ -88,7 +96,7 @@ class RSComponentRender extends Component {
 			categoryField: { types },
 		} = propsMap[component];
 
-		const fields = Object.keys(mappings).filter((field) => {
+		const fields = Object.keys(mappings).filter(field => {
 			let fieldsToCheck = [mappings[field]];
 
 			if (mappings[field].originalFields) {
@@ -134,7 +142,7 @@ class RSComponentRender extends Component {
 		});
 	};
 
-	setComponentProps = (newProps) => {
+	setComponentProps = newProps => {
 		const { componentProps } = this.state;
 		const newComponentProps = { ...componentProps, ...newProps };
 		this.setState({
@@ -148,7 +156,7 @@ class RSComponentRender extends Component {
 		}));
 	};
 
-	handleSavePreview = (values) => {
+	handleSavePreview = values => {
 		const { onPropChange, id } = this.props;
 		onPropChange(id, {
 			metaFields: values,
@@ -173,8 +181,18 @@ class RSComponentRender extends Component {
 		};
 		const code = getComponentCode(config, version);
 		return (
-			<Popover content={<pre>{code}</pre>} placement="leftTop" title="Code">
-				<Button icon="code-o" shape="circle" size="large" style={{ marginLeft: 8 }} />
+			<Popover
+				content={<pre>{code}</pre>}
+				placement="leftTop"
+				title="Code"
+			>
+				<Button
+					icon="code-o"
+					className="show-on-hover"
+					shape="circle"
+					size="large"
+					style={{ marginLeft: 8 }}
+				/>
 			</Popover>
 		);
 	};
@@ -182,17 +200,27 @@ class RSComponentRender extends Component {
 	renderFormItem = (item, name) => {
 		const { componentProps, isShopify } = this.props;
 		let FormInput = null;
-		const value = componentProps[name] === undefined ? item.default : componentProps[name];
+		const value =
+			componentProps[name] === undefined
+				? item.default
+				: componentProps[name];
 		const placeholder = item.placeholder || '';
 
-		if (name === 'customSuggestions' || (name === 'showDescription' && !isShopify)) {
+		if (
+			name === 'customSuggestions' ||
+			(name === 'showDescription' && !isShopify)
+		) {
 			return null;
 		}
 
 		switch (item.input) {
 			case 'bool': {
 				FormInput = (
-					<ToggleInput value={value} name={name} handleChange={this.setComponentProps} />
+					<ToggleInput
+						value={value}
+						name={name}
+						handleChange={this.setComponentProps}
+					/>
 				);
 				break;
 			}
@@ -222,16 +250,19 @@ class RSComponentRender extends Component {
 				if (name === 'categoryField') {
 					noOptionsMessage = (
 						<p style={{ lineHeight: '1.5' }}>
-							There are no compatible fields present in your data mappings.{' '}
-							<a href={mappingsURL}>You can edit your mappings</a> (agggregation
-							components)
+							There are no compatible fields present in your data
+							mappings.{' '}
+							<a href={mappingsURL}>You can edit your mappings</a>{' '}
+							(agggregation components)
 						</p>
 					);
 
-					this.getCategoryField().forEach(field => dropdownOptions.push({
+					this.getCategoryField().forEach(field =>
+						dropdownOptions.push({
 							label: field,
 							key: field,
-						}));
+						}),
+					);
 
 					if (dropdownOptions.length) {
 						// If no categoryfield is selected
@@ -277,9 +308,15 @@ class RSComponentRender extends Component {
 						name={name}
 						height="250px"
 						width="100%"
-						value={stateComponentProps.customSuggestions || value || undefined}
+						value={
+							stateComponentProps.customSuggestions ||
+							value ||
+							undefined
+						}
 						placeholder={placeholder}
-						onChange={value => this.setComponentProps({ [name]: value })}
+						onChange={value =>
+							this.setComponentProps({ [name]: value })
+						}
 					/>
 				);
 				break;
@@ -287,7 +324,11 @@ class RSComponentRender extends Component {
 
 			default: {
 				FormInput = (
-					<TextInput name={name} value={value} handleChange={this.setComponentProps} />
+					<TextInput
+						name={name}
+						value={value}
+						handleChange={this.setComponentProps}
+					/>
 				);
 				break;
 			}
@@ -315,7 +356,9 @@ class RSComponentRender extends Component {
 					id={id}
 					description={propNames.dataField.description}
 					componentProps={stateComponentProps}
-					getAvailableDataField={() => getAvailableDataField({ component, id, mappings })}
+					getAvailableDataField={() =>
+						getAvailableDataField({ component, id, mappings })
+					}
 					setComponentProps={this.setComponentProps}
 				/>
 				{Object.keys(propNames)
@@ -340,7 +383,11 @@ class RSComponentRender extends Component {
 			setRenderKey,
 			version,
 		} = this.props;
-		const { showModal, componentProps: stateComponentProps, previewModal } = this.state;
+		const {
+			showModal,
+			componentProps: stateComponentProps,
+			previewModal,
+		} = this.state;
 		if (!componentProps.dataField) return null;
 
 		const RSComponent = componentMap[component];
@@ -363,37 +410,69 @@ class RSComponentRender extends Component {
 			<div>
 				<Row gutter={8}>
 					{full ? (
-						<Col span={24} style={{ textAlign: 'right', paddingBottom: 16 }}>
+						<Col
+							span={24}
+							className={componentStyle}
+							style={{
+								textAlign: 'right',
+								paddingBottom: 16,
+							}}
+						>
 							<Button
 								icon="edit"
 								shape="circle"
 								size="large"
-								className={editTutorialClass}
+								className={`${
+									id === 'search' ? 'search' : ''
+								} show-on-hover edit ${editTutorialClass}`}
 								onClick={this.showModal}
 							/>
-							{showCodePreview && this.renderComponentCode()}
 							{showPreview && showCustomList ? (
 								<Button
-									icon="eye-o"
-									shape="circle"
-									size="large"
+									className="show-on-hover"
 									style={{ marginLeft: 8 }}
 									onClick={this.handlePreviewModal}
-								/>
+								>
+									<Icon type="unordered-list" />
+									Set Result View
+								</Button>
 							) : null}
+							{showCodePreview && this.renderComponentCode()}
 							{showDelete ? (
 								<Button
 									icon="delete"
 									shape="circle"
 									type="danger"
 									size="large"
+									className="show-on-hover"
 									style={{ marginLeft: 8 }}
 									onClick={() => onDelete(id)}
 								/>
 							) : null}
 						</Col>
 					) : null}
-					<Col span={full ? 24 : 20} id={id} className={tutorialClass}>
+					{full ? null : (
+						<Col
+							span={2}
+							className={componentStyle}
+							style={{ textAlign: 'right' }}
+						>
+							<Button
+								icon="edit"
+								shape="circle"
+								size="large"
+								className={`${
+									id === 'search' ? 'search' : ''
+								} show-on-hover edit ${editTutorialClass}`}
+								onClick={this.showModal}
+							/>
+						</Col>
+					)}
+					<Col
+						span={full ? 24 : 20}
+						id={id}
+						className={tutorialClass}
+					>
 						<RSComponent
 							componentId={id}
 							{...getComponentProps({
@@ -407,14 +486,11 @@ class RSComponentRender extends Component {
 						/>
 					</Col>
 					{full ? null : (
-						<Col span={4} style={{ textAlign: 'right' }}>
-							<Button
-								icon="edit"
-								shape="circle"
-								size="large"
-								className={editTutorialClass}
-								onClick={this.showModal}
-							/>
+						<Col
+							span={2}
+							className={componentStyle}
+							style={{ textAlign: 'right' }}
+						>
 							{showCodePreview && this.renderComponentCode()}
 						</Col>
 					)}
@@ -439,7 +515,11 @@ class RSComponentRender extends Component {
 						handlePreviewModal={this.handlePreviewModal}
 						handleSavePreview={this.handleSavePreview}
 						visible={previewModal}
-						dataField={generateDataField(component, componentProps.dataField, mappings)}
+						dataField={generateDataField(
+							component,
+							componentProps.dataField,
+							mappings,
+						)}
 					/>
 				) : null}
 			</div>
