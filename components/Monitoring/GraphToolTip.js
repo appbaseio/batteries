3f28cc6e08e2cdd4abc7be0e23f48f3850e73380
Lakhan Samani <lakhan.m.samani@gmail.com>
2021-01-15 22:37:29 +0530

    fix: graph tooltip styling

--- components/Monitoring/GraphToolTip.js
+++ components/Monitoring/GraphToolTip.js
@@ -4,13 +4,20 @@ import PropTypes from 'prop-types';
 const GraphToolTip = ({ active, payload, graphLabel, unit }) => {
 	if (active) {
 		return (
-			<div>
-				<div>
-					<b>Time:</b>
-					{payload[0].payload.date}
-				</div>
+			<div
+				style={{
+					background: 'rgb(250,250,250',
+					borderRadius: 5,
+					padding: 10,
+					border: '1px solid #dfdfdf',
+				}}
+			>
 				<div>
 					<b>{graphLabel}:</b> {payload[0].payload.data} {unit}
+				</div>
+				<div>
+					<b>Time: </b>
+					{payload[0].payload.date}
 				</div>
 			</div>
 		);
