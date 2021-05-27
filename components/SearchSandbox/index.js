a322617182dcf3f389a88579214e9b1eac51bc41
bietkul <kuldepsaxena155@gmail.com>
2021-05-27 19:43:49 +0530

    fix: update RS version

--- components/SearchSandbox/index.js
+++ components/SearchSandbox/index.js
@@ -324,7 +324,7 @@ class SearchSandbox extends Component {
 							react: '16.3.2',
 							'react-dom': '16.3.2',
 							antd: '^3.6.6',
-							'@appbaseio/reactivesearch': '3.12.4',
+							'@appbaseio/reactivesearch': '3.18.1',
 							'react-expand-collapse': 'latest',
 						},
 					},
