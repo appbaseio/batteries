bf0f3791aebccc6973f858c4a238083aa64b7d2a
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2022-02-19 10:45:34 +0530

    redux support for pipeliens

--- modules/actions/pipelines.js
+++ modules/actions/pipelines.js
@@ -1,16 +1,218 @@
+import yamlToJson from 'js-yaml';
+import get from 'lodash/get';
 import { createAction } from './utils';
 import AppConstants from '../constants';
-import { getPipelines as fetchPipelines } from '../../utils/app';
+import {
+	getPipelines as fetchPipelines,
+	deletePipeline as removePipeline,
+	updatePipeline,
+	createPipeline,
+	getPipelineScript,
+} from '../../utils/app';
+import { generatePipelinePayload } from '../../utils/helpers';
+
+export function getPipelinesScripts(pipelines) {
+	return (dispatch) => {
+		try {
+			dispatch(createAction(AppConstants.APP.PIPELINES.GET_SCRIPTS));
+			const scriptRefs = {};
+
+			pipelines.forEach((pipeline) => {
+				const pipelineJSON = yamlToJson.load(pipeline.content);
+				if (Array.isArray(pipelineJSON.stages) && pipelineJSON.stages.length) {
+					pipelineJSON.stages.forEach((stageItem) => {
+						if (stageItem?.scriptRef) {
+							scriptRefs[pipeline.id] = scriptRefs[pipeline.id]
+								? [...scriptRefs[pipeline.id], stageItem?.scriptRef]
+								: [stageItem?.scriptRef];
+						}
+					});
+				}
+			});
+			if (Object.keys(scriptRefs).length) {
+				const promises = [];
+				Object.keys(scriptRefs).forEach((pipelineId) => {
+					return scriptRefs[pipelineId].forEach((scriptRefName) =>
+						promises.push(getPipelineScript(pipelineId, scriptRefName)),
+					);
+				});
+				Promise.allSettled(promises)
+					.then((values) => {
+						const scriptContents = {};
+						values.forEach((val) => {
+							scriptContents[val.value.id] = {
+								...(scriptContents[val.value.id]
+									? scriptContents[val.value.id]
+									: {}),
+								[val.value.key]: {
+									content: val.value.content,
+									extension: val.value.extension,
+								},
+							};
+						});
+						return dispatch(
+							createAction(
+								AppConstants.APP.PIPELINES.GET_SCRIPTS_SUCCESS,
+								scriptContents,
+								null,
+							),
+						);
+					})
+					.catch((error) => {
+						console.log(error);
+						dispatch(
+							createAction(AppConstants.APP.PIPELINES.GET_SCRIPTS_ERROR, null, error),
+						);
+					});
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+}
 
 export function getPipelines() {
 	return (dispatch) => {
 		dispatch(createAction(AppConstants.APP.PIPELINES.GET));
 		return fetchPipelines()
 			.then((res) => {
-				dispatch(createAction(AppConstants.APP.PIPELINES.GET_SUCCESS, res, null));
+				dispatch(getPipelinesScripts(res));
+				return dispatch(createAction(AppConstants.APP.PIPELINES.GET_SUCCESS, res, null));
 			})
 			.catch((error) => {
+				console.log(error);
 				dispatch(createAction(AppConstants.APP.PIPELINES.GET_ERROR, null, error));
 			});
+	};
+}
+
+export function addPipeline(pipeline) {
+	return (dispatch) => {
+		dispatch(createAction(AppConstants.APP.PIPELINES.CREATE, { ...pipeline }));
+		return createPipeline(pipeline)
+			.then((data) =>
+				dispatch(
+					createAction(AppConstants.APP.PIPELINES.CREATE_SUCCESS, { ...data }, null),
+				),
+			)
+			.catch((error) =>
+				dispatch(createAction(AppConstants.APP.PIPELINES.CREATE_ERROR, null, error)),
+			);
+	};
+}
+
+export function putPipeline(pipeline) {
+	return (dispatch) => {
+		dispatch(createAction(AppConstants.APP.PIPELINES.UPDATE_PIPELINE, { ...pipeline }));
+		return updatePipeline(pipeline)
+			.then(() =>
+				dispatch(
+					createAction(
+						AppConstants.APP.PIPELINES.UPDATE_PIPELINE_SUCCESS,
+						{ ...pipeline },
+						null,
+					),
+				),
+			)
+			.catch((error) =>
+				dispatch(
+					createAction(
+						AppConstants.APP.PIPELINES.UPDATE_PIPELINE_ERROR,
+						{ ...pipeline },
+						error,
+					),
+				),
+			);
+	};
+}
+
+export function deletePipeline(id) {
+	return (dispatch) => {
+		dispatch(createAction(AppConstants.APP.PIPELINES.DELETE, { id }));
+		return removePipeline(id)
+			.then(() => {
+				return dispatch(
+					createAction(AppConstants.APP.PIPELINES.DELETE_SUCCESS, { id }, null),
+				);
+			})
+			.catch((error) =>
+				dispatch(createAction(AppConstants.APP.PIPELINES.DELETE_ERROR, { id }, error)),
+			);
+	};
+}
+
+export function togglePipelineStatus(pipeline) {
+	return (dispatch) => {
+		dispatch(createAction(AppConstants.APP.PIPELINES.TOGGLE_STATUS, { ...pipeline }));
+		return updatePipeline(pipeline)
+			.then(() => {
+				return dispatch(
+					createAction(
+						AppConstants.APP.PIPELINES.TOGGLE_STATUS_SUCCESS,
+						{ ...pipeline },
+						null,
+					),
+				);
+			})
+			.catch((error) =>
+				dispatch(
+					createAction(
+						AppConstants.APP.PIPELINES.TOGGLE_STATUS_ERROR,
+						{ ...pipeline },
+						error,
+					),
+				),
+			);
+	};
+}
+export function clonePipeline(pipeline, newPipeline) {
+	return (dispatch) => {
+		dispatch(createAction(AppConstants.APP.PIPELINES.CLONE, { ...pipeline }));
+		return createPipeline(newPipeline)
+			.then((data) => {
+				return dispatch(
+					createAction(AppConstants.APP.PIPELINES.CLONE_SUCCESS, { ...data }, null, {
+						...pipeline,
+					}),
+				);
+			})
+			.catch((error) =>
+				dispatch(
+					createAction(AppConstants.APP.PIPELINES.CLONE_ERROR, { ...pipeline }, error),
+				),
+			);
+	};
+}
+
+export function reorderPipelines({ id, priority }) {
+	return (dispatch, getState) => {
+		const state = getState();
+		const pipeline = get(state, '$getAppPipelines.results').find((item) => item.id === id);
+		const pipelineScripts = get(state, '$getAppPipelines.scriptResults')?.[pipeline.id];
+		const modifiedPipelineValue = yamlToJson.dump({
+			...yamlToJson.load(pipeline.content),
+			priority,
+		});
+		const pipelinePayload = generatePipelinePayload(
+			modifiedPipelineValue,
+			pipelineScripts,
+			'content',
+		);
+		dispatch(createAction(AppConstants.APP.PIPELINES.REORDER, { id, priority }));
+		return updatePipeline({ id, pipelinePayload })
+			.then(() => {
+				dispatch(
+					createAction(
+						AppConstants.APP.PIPELINES.REORDER_SUCCESS,
+						{ id, priority },
+						null,
+					),
+				);
+			})
+			.catch((error) =>
+				dispatch(
+					createAction(AppConstants.APP.PIPELINES.REORDER_ERROR, { id, priority }, error),
+				),
+			);
 	};
 }
--- modules/reducers/getAppPipelines.js
+++ modules/reducers/getAppPipelines.js
@@ -24,6 +24,280 @@ function getAppPipelines(state = initialAppState, action) {
 				error: action.payload,
 			};
 
+		case AppConstants.APP.PIPELINES.GET_SCRIPTS:
+			return {
+				...state,
+				isFetchingPipelineScripts: true,
+				scriptFetchSuccess: false,
+			};
+		case AppConstants.APP.PIPELINES.GET_SCRIPTS_SUCCESS:
+			return {
+				...state,
+				isFetchingPipelineScripts: false,
+				scriptFetchSuccess: true,
+				scriptFetchError: false,
+				scriptResults: action.payload,
+			};
+		case AppConstants.APP.PIPELINES.GET_SCRIPTS_ERROR:
+			return {
+				...state,
+				isFetchingPipelineScripts: false,
+				scriptFetchSuccess: true,
+				scriptFetchError: action.payload,
+			};
+
+		case AppConstants.APP.PIPELINES.CREATE: {
+			return {
+				...state,
+				create: {
+					isLoading: true,
+					error: null,
+				},
+			};
+		}
+		case AppConstants.APP.PIPELINES.CREATE_SUCCESS: {
+			return {
+				...state,
+				results: [...state.results, { ...action.payload }],
+				create: {
+					isLoading: false,
+					error: null,
+				},
+			};
+		}
+		case AppConstants.APP.PIPELINES.CREATE_ERROR: {
+			return {
+				...state,
+				create: {
+					isLoading: false,
+					error: action.error,
+				},
+			};
+		}
+
+		case AppConstants.APP.PIPELINES.TOGGLE_STATUS: {
+			const updatedResults = state.results.map((item) =>
+				item.id === action.payload.id
+					? {
+							...item,
+							isToggling: true,
+					  }
+					: item,
+			);
+			return {
+				...state,
+				results: updatedResults,
+			};
+		}
+		case AppConstants.APP.PIPELINES.TOGGLE_STATUS_SUCCESS: {
+			const updatedResults = state.results.map((item) =>
+				item.id === action.payload.id
+					? {
+							...item,
+							enabled: action.payload.enabled,
+							isToggling: false,
+					  }
+					: item,
+			);
+			return {
+				...state,
+				results: updatedResults,
+			};
+		}
+		case AppConstants.APP.PIPELINES.TOGGLE_STATUS_ERROR: {
+			const updatedResults = state.results.map((item) =>
+				item.id === action.payload.id
+					? {
+							...item,
+							isToggling: false,
+							toggleError: action.error,
+					  }
+					: item,
+			);
+			return {
+				...state,
+				results: updatedResults,
+			};
+		}
+
+		case AppConstants.APP.PIPELINES.UPDATE_PIPELINE: {
+			const updatedResults = state.results.map((item) =>
+				item.id === action.payload.id
+					? {
+							...item,
+							update: {
+								isLoading: true,
+								error: null,
+							},
+					  }
+					: item,
+			);
+			return {
+				...state,
+				results: updatedResults,
+			};
+		}
+		case AppConstants.APP.PIPELINES.UPDATE_PIPELINE_SUCCESS: {
+			const updatedResults = state.results.map((item) =>
+				item.id === action.payload.id
+					? {
+							...item,
+							...action.payload,
+							update: {
+								isLoading: false,
+								error: null,
+							},
+					  }
+					: item,
+			);
+			return {
+				...state,
+				results: updatedResults,
+			};
+		}
+		case AppConstants.APP.PIPELINES.UPDATE_PIPELINE_ERROR: {
+			const updatedResults = state.results.map((item) =>
+				item.id === action.payload.id
+					? {
+							...item,
+							update: {
+								isLoading: false,
+								error: action.error,
+							},
+					  }
+					: item,
+			);
+			return {
+				...state,
+				results: updatedResults,
+			};
+		}
+
+		case AppConstants.APP.PIPELINES.DELETE: {
+			const updatedResults = state.results.map((item) =>
+				item.id === action.payload.id
+					? {
+							...item,
+							isDeleting: true,
+					  }
+					: item,
+			);
+			return {
+				...state,
+				results: updatedResults,
+			};
+		}
+		case AppConstants.APP.PIPELINES.DELETE_SUCCESS: {
+			const filteredResults = state.results.filter((item) => item.id !== action.payload.id);
+			return {
+				...state,
+				results: filteredResults,
+				deleted: action.payload.id,
+			};
+		}
+		case AppConstants.APP.PIPELINES.DELETE_ERROR: {
+			const updatedResults = state.results.map((item) =>
+				item.id === action.payload.id
+					? {
+							...item,
+							isDeleting: false,
+							deleteError: action.error,
+					  }
+					: item,
+			);
+			return {
+				...state,
+				results: updatedResults,
+			};
+		}
+		case AppConstants.APP.PIPELINES.CLONE: {
+			const updatedResults = state.results.map((item) =>
+				item.id === action.payload.id
+					? {
+							...item,
+							isCloning: true,
+					  }
+					: item,
+			);
+			return {
+				...state,
+				results: updatedResults,
+			};
+		}
+		case AppConstants.APP.PIPELINES.CLONE_SUCCESS: {
+			const updatedResults = state.results.map((item) =>
+				item.id === action.meta.id
+					? {
+							...item,
+							isCloning: false,
+					  }
+					: item,
+			);
+			return {
+				...state,
+				results: [...updatedResults, { ...action.payload }],
+			};
+		}
+		case AppConstants.APP.PIPELINES.CLONE_ERROR: {
+			const updatedResults = state.results.map((item) =>
+				item.id === action.payload.id
+					? {
+							...item,
+							isCloning: false,
+							cloneError: action.error,
+					  }
+					: item,
+			);
+			return {
+				...state,
+				results: updatedResults,
+			};
+		}
+
+		case AppConstants.APP.PIPELINES.REORDER: {
+			const { id, priority } = action.payload;
+			const reorderedPipelines = state.results.map((pipeline) => {
+				if (pipeline.id === id) {
+					return {
+						...pipeline,
+						priority,
+					};
+				}
+
+				return pipeline;
+			});
+			return {
+				...state,
+				results: reorderedPipelines,
+				reordering: true,
+			};
+		}
+		case AppConstants.APP.PIPELINES.REORDER_SUCCESS: {
+			return {
+				...state,
+				reordering: false,
+				error: null,
+			};
+		}
+		case AppConstants.APP.PIPELINES.REORDER_ERROR: {
+			const { id, priority } = action.payload;
+			const revertedPipelines = state.results.map((pipeline) => {
+				if (pipeline.id === id) {
+					return {
+						...pipeline,
+						priority,
+					};
+				}
+				return pipeline;
+			});
+			return {
+				...state,
+				results: revertedPipelines,
+				reordering: false,
+				error: action.error,
+			};
+		}
+
 		default:
 			return state;
 	}
