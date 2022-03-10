81976ae57479f5f3ad589ff772b57bfc4db1ca04
Siddharth Kothari <sids.aquarius@gmail.com>
2022-03-10 22:57:46 +0530

    feat(monitoring): add ES v8 metricbeat index to search on

--- components/Monitoring/MonitoringContainer.js
+++ components/Monitoring/MonitoringContainer.js
@@ -38,14 +38,17 @@ const MonitoringContainer = ({
 		let isMounted = true;
 		const getData = async () => {
 			try {
-				const res = await fetch(`${esURL}/metricbeat-*`, {
-					headers: {
-						Authorization: `Basic ${btoa(
-							`${esUsername}:${esPassword}`,
-						)}`,
-						'Content-Type': 'application/json',
+				const res = await fetch(
+					`${esURL}/metricbeat-*,.ds-metricbeat-*`,
+					{
+						headers: {
+							Authorization: `Basic ${btoa(
+								`${esUsername}:${esPassword}`,
+							)}`,
+							'Content-Type': 'application/json',
+						},
 					},
-				});
+				);
 				const json = await res.json();
 				if (Object.keys(json).length && isMounted) {
 					setConfig({
