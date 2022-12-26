b0b2edf04c05818fa82352a5b293d9622d82ca54
Mohd Ashraf <57627350+mohdashraf010897@users.noreply.github.com>
2022-12-26 12:34:01 +0530

    #dev_testing change ACC_API URL

--- utils/index.js
+++ utils/index.js
@@ -12,7 +12,7 @@ export const isStaging = false;
 
 export const ACC_API = isStaging
 	? 'https://accapi-staging.reactiveapps.io'
-	: 'https://accapi.appbase.io';
+	: 'http://accapi-prod.reactivesearch.io';
 
 // export const ACC_API = 'http://localhost:3000';
 export const SCALR_API = isStaging
