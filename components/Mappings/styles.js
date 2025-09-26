ca355286f258bd1de2fae48c6d05cbb308b70187
Siddharth Kothari <sids.aquarius@gmail.com>
2025-09-26 18:16:47 +0530

    chore: lint changes

--- components/Mappings/styles.js
+++ components/Mappings/styles.js
@@ -196,7 +196,7 @@ export const Button = styled('a')`
 	align-items: center;
 	color: #53683b;
 	font-weight: 600;
-	background-color: ${props => (props.ghost ? grey : green)};
+	background-color: ${(props) => (props.ghost ? grey : green)};
 	text-transform: uppercase;
 	box-shadow: 0 3px 3px 0 rgba(0, 0, 0, 0.1);
 	cursor: pointer;
@@ -204,7 +204,7 @@ export const Button = styled('a')`
 
 	&:hover,
 	&:focus {
-		background-color: ${props => (props.ghost ? greyHover : greenHover)};
+		background-color: ${(props) => (props.ghost ? greyHover : greenHover)};
 		box-shadow: 0 5px 5px 0 rgba(0, 0, 0, 0.1);
 		text-decoration: none;
 	}
